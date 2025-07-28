7b373d63a3f49f9ce5ea24440d25bfbab54e9280
ITNguyen9x <nguyenhohoangvy@gmail.com>
2025-07-28 22:01:41 +0700

    .

--- assets/MaChuongTrinh/DichVu/TaoBuocDi.ts
+++ assets/MaChuongTrinh/DichVu/TaoBuocDi.ts
@@ -1,28 +1,78 @@
-import { EventTouch, Node } from "cc";
+import { EventTouch, instantiate, Node, Prefab, Vec2 } from "cc";
 import { NuocDi, QuanCo } from "../QuanCo/QuanCoModel";
 
 export class TaoBuocDi{
+
+    static daiRongO: number = 64;
+    static soGiaY: number = 16;
 
     static ChonQuanCo(danhSachQuanCo: QuanCo[], onSelect: (quanCo: QuanCo) => void) { danhSachQuanCo.forEach(ds => this.SuKienChonQuan(ds, onSelect)) }
     static SuKienChonQuan(quanCo: QuanCo, onSelect: (quanCo: QuanCo) => void) {
         quanCo.node.on(Node.EventType.TOUCH_START, (event: EventTouch) => { onSelect(quanCo) });
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
     static ChonNuocDi(danhSachNuocDi: Node[], onSelect: (nuocDi: Node) => void) { danhSachNuocDi.forEach(ds => this.SuKienChonNuocDi(ds, onSelect)) }
     static SuKienChonNuocDi(nuocDi: Node, onSelect: (nuocDi: Node) => void) {
         nuocDi.on(Node.EventType.TOUCH_START, (event: EventTouch) => { onSelect(nuocDi) });
     }
+
+
+    static TaoBuocDiCuaTuong(quanCo: QuanCo, dsQuanCo: QuanCo[], diChuyenMau: Prefab, diChuyen: Node): Node[] {
+        let dsNuocDi : Node[] = [];
+        if(this.KiemTraBuocDiBenTren(quanCo, dsQuanCo)){
+            quanCo.hang++;
+            dsNuocDi.push(this.TaoBuocDi(quanCo, diChuyenMau, diChuyen));
+            quanCo.hang--;
+        }
+        if(this.KiemTraBuocDiBenDuoi(quanCo, dsQuanCo)){
+            quanCo.hang--;
+            dsNuocDi.push(this.TaoBuocDi(quanCo, diChuyenMau, diChuyen));
+            quanCo.hang++;
+        }
+        if(this.KiemTraBuocDiBenPhai(quanCo, dsQuanCo)){
+            quanCo.cot++;
+            dsNuocDi.push(this.TaoBuocDi(quanCo, diChuyenMau, diChuyen));
+            quanCo.cot--;
+        }
+        if(this.KiemTraBuocDiBenTrai(quanCo, dsQuanCo)){
+            quanCo.cot--;
+            dsNuocDi.push(this.TaoBuocDi(quanCo, diChuyenMau, diChuyen));
+            quanCo.cot++;
+        }
+        return dsNuocDi;
+    }
+
+    static TaoBuocDi(quanCo: QuanCo, diChuyenMau: Prefab, diChuyen: Node): Node{
+        let vitri = this.LayViTri(quanCo.hang, quanCo.cot);
+        let node: Node = instantiate(diChuyenMau);
+        node.setPosition(vitri.x, vitri.y - this.soGiaY);
+        return node;
+    }
+
+    static KiemTraBuocDiBenTren(quanCo: QuanCo, dsQuanCo: QuanCo[]): boolean {
+        if(this.GoiHanBanCo(quanCo.hang + 1, quanCo.cot)) return false;
+        return !dsQuanCo.some(q => q.cot == quanCo.cot && q.hang === quanCo.hang + 1 && q.phe == quanCo.phe);
+    }
+    static KiemTraBuocDiBenDuoi(quanCo: QuanCo, dsQuanCo: QuanCo[]): boolean {
+        if(this.GoiHanBanCo(quanCo.hang - 1, quanCo.cot)) return false;
+        return !dsQuanCo.some(q => q.cot == quanCo.cot && q.hang === quanCo.hang - 1 && q.phe == quanCo.phe);
+    }
+    static KiemTraBuocDiBenPhai(quanCo: QuanCo, dsQuanCo: QuanCo[]): boolean {
+        if(this.GoiHanBanCo(quanCo.hang, quanCo.cot + 1)) return false;
+        return !dsQuanCo.some(q => q.cot == quanCo.cot + 1 && q.hang === quanCo.hang && q.phe == quanCo.phe);
+    }
+    static KiemTraBuocDiBenTrai(quanCo: QuanCo, dsQuanCo: QuanCo[]): boolean {
+        if(this.GoiHanBanCo(quanCo.hang, quanCo.cot - 1)) return false;
+        return !dsQuanCo.some(q => q.cot == quanCo.cot - 1 && q.hang === quanCo.hang && q.phe == quanCo.phe);
+    }
+    static GoiHanBanCo(hang: number, cot: number): boolean{
+        let hangMin: number = 0;
+        let cotMin: number = 0;
+        let hangMax: number = 9;
+        let cotMax: number = 10;
+        if(hang < hangMin || hang > hangMax || cot < cotMin || cot > cotMax) return true;
+        else return false;
+    }
+    static LayViTri(hang: number, cot: number): Vec2 { return new Vec2((cot - 4) * this.daiRongO, (hang - 4) * this.daiRongO); }
 
 }
--- assets/MaChuongTrinh/QuanCo/QuanCoComponent.ts
+++ assets/MaChuongTrinh/QuanCo/QuanCoComponent.ts
@@ -46,8 +46,8 @@ export class QuanCoComponent extends Component {
 
     hang: number = 10;
     cot: number = 9;
-    daiRongO: number = 64;
-    soGiaY: number = 16;
+    
+    
     nuocDi: { hang: number, cot: number }[] = [];
     tatCaQuanCo: QuanCo[] = [];
     isChonQuan : boolean = false;
@@ -61,7 +61,7 @@ export class QuanCoComponent extends Component {
     TaoTatCaQuanCo(){
         this.tatCaQuanCo = TaoQuanCo.TaoToanQuanCo(this.tuongXanhMau, this.siXanhMau, this.boXanhMau, this.nguaXanhMau, this.xeXanhMau, this.phaoXanhMau,
             this.chotXanhMau, this.tuongDoMau, this.siDoMau, this.boDoMau, this.nguaDoMau, this.xeDoMau, this.phaoDoMau, this.chotDoMau);
-        this.tatCaQuanCo.forEach((x: any) => x.node.setParent(this.quanCo));
+        this.tatCaQuanCo.forEach((quanCo: any) => quanCo.node.setParent(this.quanCo));
     }
 
     ChonQuanCo(){ TaoBuocDi.ChonQuanCo(this.tatCaQuanCo, (quanCo) => this.ChonQuan(quanCo)) }
@@ -77,7 +77,7 @@ export class QuanCoComponent extends Component {
         }
         switch(quanCo.loai){
             case LoaiQuan.Tuong:
-                this.KiemTraBuocDiCuaTuong(quanCo);
+                TaoBuocDi.TaoBuocDiCuaTuong(quanCo, this.tatCaQuanCo, this.diChuyenMau, this.diChuyen).forEach((nuocDi: Node) =>{ nuocDi.setParent(this.diChuyen)});
                 this.ChonNuocCo();
                 break;
             default:
@@ -90,66 +90,12 @@ export class QuanCoComponent extends Component {
 
     }
 
-    KiemTraBuocDiCuaTuong(quanCo: QuanCo) {
-        if(this.kiemTraQuanCoBenTren(quanCo)){
-            //let bentren: QuanCo = quanCo;
-            quanCo.hang++;
-            this.TaoBuocDi(quanCo);
-            quanCo.hang--;
-        }
-        if(this.kiemTraQuanCoBenDuoi(quanCo)){
-            //let benduoi: QuanCo = quanCo;
-            quanCo.hang--;
-            this.TaoBuocDi(quanCo);
-            quanCo.hang++;
-        }
-        if(this.kiemTraQuanCoBenPhai(quanCo)){
-            //let benphai: QuanCo = quanCo;
-            quanCo.cot++;
-            this.TaoBuocDi(quanCo);
-            quanCo.cot--;
-        }
-        if(this.kiemTraQuanCoBenTrai(quanCo)){
-            //let bentrai: QuanCo = quanCo;
-            quanCo.cot--;
-            this.TaoBuocDi(quanCo);
-            quanCo.cot++;
-        }
-    }
+    
 
-    TaoBuocDi(quanCo: QuanCo){
-        let vitri = this.LayViTri(quanCo.hang, quanCo.cot);
-        let node: Node = instantiate(this.diChuyenMau);
-        node.setPosition(vitri.x, vitri.y - this.soGiaY);
-        node.setParent(this.diChuyen);
-        this.dsNuocDi.push(node);
-    }
+    
+    
 
-    LayViTri(hang: number, cot: number): Vec2 { return new Vec2((cot - 4) * this.daiRongO, (hang - 4) * this.daiRongO); }
+    
 
-    GoiHanBanCo(hang: number, cot: number): boolean{
-        let hangMin: number = 0;
-        let cotMin: number = 0;
-        let hangMax: number = 9;
-        let cotMax: number = 10;
-        if(hang < hangMin || hang > hangMax || cot < cotMin || cot > cotMax) return true;
-        else return false;
-    }
-
-    kiemTraQuanCoBenTren(quanCo: QuanCo): boolean {
-        if(this.GoiHanBanCo(quanCo.hang + 1, quanCo.cot)) return false;
-        return !this.tatCaQuanCo.some(q => q.cot == quanCo.cot && q.hang === quanCo.hang + 1 && q.phe == quanCo.phe);
-    }
-    kiemTraQuanCoBenDuoi(quanCo: QuanCo): boolean {
-        if(this.GoiHanBanCo(quanCo.hang - 1, quanCo.cot)) return false;
-        return !this.tatCaQuanCo.some(q => q.cot == quanCo.cot && q.hang === quanCo.hang - 1 && q.phe == quanCo.phe);
-    }
-    kiemTraQuanCoBenPhai(quanCo: QuanCo): boolean {
-        if(this.GoiHanBanCo(quanCo.hang, quanCo.cot + 1)) return false;
-        return !this.tatCaQuanCo.some(q => q.cot == quanCo.cot + 1 && q.hang === quanCo.hang && q.phe == quanCo.phe);
-    }
-    kiemTraQuanCoBenTrai(quanCo: QuanCo): boolean {
-        if(this.GoiHanBanCo(quanCo.hang, quanCo.cot - 1)) return false;
-        return !this.tatCaQuanCo.some(q => q.cot == quanCo.cot - 1 && q.hang === quanCo.hang && q.phe == quanCo.phe);
-    }
+    
 }
